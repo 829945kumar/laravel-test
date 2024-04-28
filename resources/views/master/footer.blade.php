c5217897ab9835801cab6c2e8a9d04b42410dbd4
829945kumar <829945kumar@gmail.com>
2024-04-28 16:12:19 +0530

    add home page with using yield

---
+++ resources/views/master/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+    <!-- place footer here -->
+    <h1>footer paste here</h1>
+</footer>
---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,19 @@
+@include("master.navbar")
+<main>
+@yield("slice")
+</main>
+
+@include("master.footer")        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
