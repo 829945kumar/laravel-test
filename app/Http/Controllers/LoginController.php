5d16dcac9f892825f3c58814f71dbdf56b472199
829945kumar <829945kumar@gmail.com>
2024-04-30 22:00:36 +0530

    form validation with own syntax

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,10 +12,15 @@ class LoginController extends Controller
 
     public function handlelogin(Request $request ){
         $request->validate([
-            'name' => 'required',
-            'email'=> 'required',
+            'name' => ['required','alpha','min:6','max:10'],
+            'email'=> ['required','email'],
             'password'=> 'required'
-        ]);
+        ],
+        [
+            'name.required' => 'The User name is required!',
+            'name.alpha'=> 'Usser name must be contain letters'
+        ]
+    );
         return $request;
     }
 }
